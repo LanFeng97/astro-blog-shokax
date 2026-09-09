a1fd1021207ad5f329a3fd34e78cff638bb9e760
Smartre97 <1192967545@qq.com>
2026-09-10 00:33:29 +0800

    变更站点信息

--- src/theme.config.ts
+++ src/theme.config.ts
@@ -3,8 +3,38 @@ import { defineConfig } from "./toolkit/themeConfig";
 
 export default defineConfig({
 //网站默认语言
-  siteName: "My Blog",
+  siteName: "My Blog",//左上角标题栏名字
   locale: "zh-CN",   // zh-CN | zh-TW | ja | en
+
+//首页大标题
+    brand: {
+    title: "轻沣的航海日志",
+    subtitle: "技术与简单日常记录",
+    logo: "✨",
+  },
+
+//侧边栏
+  sidebar: {
+    author: "轻沣",
+    description: "独立游戏开发中",
+    //社交配置
+    social: {
+      github: {
+        url: "https://github.com/LanFeng97",
+        icon: "i-ri-github-fill",
+      },
+      twitter: {
+        url: "https://twitter.com/yourname",
+        icon: "i-ri-twitter-x-line",
+      },
+      email: {
+        url: "mailto:your@email.com",
+        icon: "i-ri-mail-line",
+      },
+    },
+  },
+
+
 
 
 });
